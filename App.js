a93605947501dfb72b5c870973ae4a5dde5458b7
kelvinchua1998 <kelvin_chua_1998@hotmail.com>
2021-03-09 01:27:44 +0800

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  console.log("ap executed");
+  const handlePress = () => console.log("button pressed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View style={{ backgroundColor: "dodgerblue", flex: 3 }} />
+      <View style={{ backgroundColor: "gold", flex: 2 }} />
+      <View style={{ backgroundColor: "orange", flex: 1 }} />
     </View>
   );
 }
@@ -14,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
