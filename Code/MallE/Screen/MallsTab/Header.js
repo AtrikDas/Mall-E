7c27cc92b8435f885f5c98f096c4ab08722a6d65
brygoh <72204360+brygoh@users.noreply.github.com>
2021-03-14 23:04:47 +0800

    Header

---
+++ Code/MallE/Screen/MallsTab/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {LinearGradient} from 'react-native-linear-gradient';
+import {Icon, SearchBar} from 'react-native-elements';
+
+export default class Header extends React.Component {
+    render() {
+        return(
+            <View style = {styles.header}>
+                <LinearGradient colors={['#047580', '#A5F2F9']}
+                style={styles.headerBackground}>
+                    <Text style={{fontFamily: 'monospace' ,fontSize: 30, fontWeight: 'bold', color: 'white'}}>Mall</Text>
+                    <View style = {styles.search}>
+                        <Icon name="list"
+                            type="feather"
+                            size={40}>
+                        </Icon>
+                        <SearchBar placeholder="Type Here..."
+                            style={styles.searchBox}
+                            lightTheme="true"
+                            platform="ios"
+                            containerStyle={{backgroundColor: ['#047580', '#A5F2F9'], borderWidth: 0, width: "85%"}}>
+                        </SearchBar>
+                    </View>
+                </LinearGradient>
+            </View>
+            )
+    }
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    search: {
+        width: '100%',
+        flexDirection: 'row',
+        padding: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    headerBackground: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
