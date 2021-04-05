05d8f1d5630b363e3707a65d55f390336a02b574
Atrik Das <atrikdas@gmail.com>
2021-04-06 01:10:15 +0800

    changed to old bookmark

--- Code/MallE/Screen/ProfileScreens/Profile.js
+++ Code/MallE/Screen/ProfileScreens/Profile.js
@@ -1,83 +1,128 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity
-} from 'react-native';
-import {
-  Avatar,
-  Title,
-  Caption,
+import React, { useState, useEffect, Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Avatar, Title, Caption, TouchableRipple, } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-community/async-storage';
+import { Container, Header, Content, Button, Card, CardItem, Body } from 'native-base';
 
-  TouchableRipple,
-} from 'react-native-paper';
-
-import { Button } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default class Profile extends Component {
-  render() {
+const Profile = () => {
+
+  const [name, setName] = useState([]);
+
+  onPressReload = async () => {
+    var value = await AsyncStorage.getItem('bookmarks');
+    value = value.replace(/[\[\]"]+/g, '');
+    var array = value.split(",")
+    setName(array)
+    console.log(name)
+  }
+  onPressDelete = async () => {
+    var value = await AsyncStorage.removeItem('bookmarks');
+    var array = [];
+    setName(array)
+    console.log(name)
+  }
+
+  {/*render()*/ }
+  {
     return (
-      <View style={styles.container}>
-        <View style={styles.header}></View>
-        <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
-        <View style={styles.body}>
-          <View style={styles.bodyContent}>
-            <Text style={styles.name}>Abhinandan</Text>
-          </View>
-          <View style={styles.userInfoSection}>
-            <View style={styles.row}>
-              <Icon name="map-marker-radius" color="#777777" size={25} />
-              <Text style={{ color: "#777777", marginLeft: 20, fontSize: 16, }}>Singapore</Text>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>Abhinandan</Text>
             </View>
-            <View style={styles.row}>
-              <Icon name="phone" color="#777777" size={25} />
-              <Text style={{ color: "#777777", marginLeft: 20, fontSize: 16, }}>+65 8661 2141</Text>
+            <View style={styles.userInfoSection}>
+              <View style={styles.row}>
+                <Icon name="map-marker-radius" color="#777777" size={25} />
+                <Text style={{ color: "#777777", marginLeft: 20, fontSize: 16, }}>Singapore</Text>
+              </View>
+              <View style={styles.row}>
+                <Icon name="phone" color="#777777" size={25} />
+                <Text style={{ color: "#777777", marginLeft: 20, fontSize: 16, }}>+65 8652 0943</Text>
+              </View>
+              <View style={styles.row}>
+                <Icon name="email" color="#777777" size={25} />
+                <Text style={{ color: "#777777", marginLeft: 20, fontSize: 16 }}>abhinandan@gmail.com</Text>
+              </View>
             </View>
-            <View style={styles.row}>
-              <Icon name="email" color="#777777" size={25} />
-              <Text style={{ color: "#777777", marginLeft: 20, fontSize: 16 }}>abhinandan@gmail.com</Text>
+
+            <Card>
+              <CardItem header bordered button onPress={onPressReload}>
+
+                <Icon name="book-outline" color="#00BFFF" size={25} />
+                <Text style={styles.menuItemText}>Show Bookmarks</Text>
+
+              </CardItem>
+              <CardItem bordered>
+                <Body>
+                  <Text
+                    style={{
+                      fontSize: 15,
+                      textAlign: 'left',
+                    }}>
+                    {name.map(place => { return (<Text>{place} {"\n"}</Text>) })}
+                  </Text>
+                </Body>
+              </CardItem>
+              <CardItem footer bordered button onPress={onPressDelete}>
+
+                <Icon name="delete" color="#00BFFF" size={25} />
+                <Text style={styles.menuItemText}>Delete Bookmarks</Text>
+
+              </CardItem>
+            </Card>
+
+
+            <View style={styles.menuWrapper}>
+
+              {/*<TouchableOpacity onPress={onPressReload}>
+                <View style={styles.menuItem}>
+                  <Icon name="bookmark" color="#00BFFF" size={25} />
+                  <Text style={styles.menuItemText}>Show BookMarks</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => { }}>
+                <View style={styles.menuItem}>
+                  <Icon name="account-edit" color="#00BFFF" size={25} />
+                  <Text style={styles.menuItemText}>Edit Profile</Text>
+                </View>
+                  </TouchableOpacity>*/}
+
+              <TouchableOpacity onPress={onPressReload}>
+                <View style={styles.menuItem}>
+                  <Icon name="eye" color="#00BFFF" size={25} />
+                  <Text style={styles.menuItemText}>Privacy</Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => { }}>
+                <View style={styles.menuItem}>
+                  <Icon name="cog-outline" color="#00BFFF" size={25} />
+                  <Text style={styles.menuItemText}>Settings</Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => { }}>
+                <View style={styles.menuItem}>
+                  <Icon name="information" color="#00BFFF" size={25} />
+                  <Text style={styles.menuItemText}>Help and Support</Text>
+                </View>
+              </TouchableOpacity>
             </View>
-          </View>
-          <View style={styles.menuWrapper}>
-            <TouchableOpacity onPress={() => { }}>
-              <View style={styles.menuItem}>
-                <Icon name="bookmark" color="#00BFFF" size={25} />
-                <Text style={styles.menuItemText}>Your Bookmarks</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { }}>
-              <View style={styles.menuItem}>
-                <Icon name="account-edit" color="#00BFFF" size={25} />
-                <Text style={styles.menuItemText}>Edit Profile</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { }}>
-              <View style={styles.menuItem}>
-                <Icon name="eye" color="#00BFFF" size={25} />
-                <Text style={styles.menuItemText}>Privacy</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { }}>
-              <View style={styles.menuItem}>
-                <Icon name="cog-outline" color="#00BFFF" size={25} />
-                <Text style={styles.menuItemText}>Settings</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { }}>
-              <View style={styles.menuItem}>
-                <Icon name="information" color="#00BFFF" size={25} />
-                <Text style={styles.menuItemText}>Help and Support</Text>
-              </View>
-            </TouchableOpacity>
           </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
+
+export default Profile;
 
 const styles = StyleSheet.create({
   header: {
